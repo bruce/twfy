1e222317db20e24a70886db3ee24e49dade1551a
Bruce Williams <brwcodes@gmail.com>
2013-11-12 15:10:51 -0800

    Cleanup some syntax in client.rb

--- lib/twfy/client.rb
+++ lib/twfy/client.rb
@@ -21,85 +21,96 @@ module Twfy
       @logger.send(level, message) if $DEBUG
     end
 
-    def convert_url(params={})
-      service :convertURL, validate(params, :require => :url) do |value|
+    def convert_url(params = {})
+      service :convertURL, validate(params, require: :url) do |value|
         URI.parse(value['url'])
       end
     end
 
-    def constituency(params={})
-      service :getConstituency, validate(params, :require => :postcode), Constituency
+    def constituency(params = {})
+      service :getConstituency, validate(params,
+                                         require: :postcode), Constituency
     end
 
-    def constituencies(params={})
-      service :getConstituencies, validate(params, :allow => [:date, :search, :longitude, :latitude, :distance]), Constituency
+    def constituencies(params = {})
+      service :getConstituencies, validate(params,
+                                           allow: [:date, :search, :longitude, :latitude, :distance]), Constituency
     end
 
-    def mp(params={})
-      service :getMP, validate(params, :allow => [:postcode, :constituency, :id, :always_return]), MP
+    def mp(params = {})
+      service :getMP, validate(params,
+                               allow: [:postcode, :constituency, :id, :always_return]), MP
     end
 
-    def mp_info(params={})
-      service :getMPInfo, validate(params, :require => :id)
+    def mp_info(params = {})
+      service :getMPInfo, validate(params, require: :id)
     end
 
-    def mps(params={})
-      service :getMPs, validate(params, :allow => [:date, :party, :search]), MP
+    def mps(params = {})
+      service :getMPs, validate(params, allow: [:date, :party, :search]), MP
     end
 
-    def lord(params={})
-      service :getLord, validate(params, :require => :id)
+    def lord(params = {})
+      service :getLord, validate(params, require: :id)
     end
 
-    def lords(params={})
-      service :getLords, validate(params, :allow => [:date, :party, :search])
+    def lords(params = {})
+      service :getLords, validate(params, allow: [:date, :party, :search])
     end
 
     # Members of Legislative Assembly
-    def mlas(params={})
-      service :getMLAs, validate(params, :allow => [:date, :party, :search])
+    def mlas(params = {})
+      service :getMLAs, validate(params, allow: [:date, :party, :search])
     end
 
     # Member of Scottish parliament
-    def msps(params={})
-      service :getMSPs, validate(params, :allow => [:date, :party, :search])
+    def msps(params = {})
+      service :getMSPs, validate(params, allow: [:date, :party, :search])
     end
 
-    def geometry(params={})
-      service :getGeometry, validate(params, :allow => :name), Geometry
+    def geometry(params = {})
+      service :getGeometry, validate(params, allow: :name), Geometry
     end
 
-    def committee(params={})
-      service :getCommittee, validate(params, :require => :name, :allow => :date)
+    def committee(params = {})
+      service :getCommittee, validate(params, require: :name, allow: :date)
     end
 
-    def debates(params={})
-      service :getDebates, validate(params, :require => :type, :require_one_of => [:date, :person, :search, :gid], :allow_dependencies => {
-                                      :search => [:order, :page, :num],
-                                      :person => [:order, :page, :num]
+    def debates(params = {})
+      service :getDebates, validate(params,
+                                    require: :type,
+                                    require_one_of: [:date, :person, :search, :gid],
+                                    allow_dependencies: {
+                                      search: [:order, :page, :num],
+                                      person: [:order, :page, :num]
                                     })
     end
 
 
-    def wrans(params={})
-      service :getWrans, validate(params, :require_one_of => [:date, :person, :search, :gid], :allow_dependencies => {
-                                    :search => [:order, :page, :num],
-                                    :person => [:order, :page, :num]
+    def wrans(params = {})
+      service :getWrans, validate(params,
+                                  require_one_of: [:date, :person, :search, :gid],
+                                  allow_dependencies: {
+                                    search: [:order, :page, :num],
+                                    person: [:order, :page, :num]
                                   } )
     end
 
-    def wms(params={})
-      service :getWMS, validate(params, :require_one_of => [:date, :person, :search, :gid], :allow_dependencies => {
-                                  :search => [:order, :page, :num],
-                                  :person => [:order, :page, :num]
+    def wms(params = {})
+      service :getWMS, validate(params,
+                                require_one_of: [:date, :person, :search, :gid],
+                                allow_dependencies: {
+                                  search: [:order, :page, :num],
+                                  person: [:order, :page, :num]
                                 } )
     end
 
-    def comments(params={})
-      service :getComments, validate(params, :allow => [:date, :search, :user_id, :pid, :page, :num])
+    def comments(params = {})
+      service :getComments, validate(params,
+                                     allow: [:date, :search, :user_id, :pid, :page, :num])
     end
 
-    def service(name, params={}, target=OpenStruct, &block)
+    def service(name, params = {}, target = OpenStruct, &block)
       log "Calling service #{name}"
       url = BASE + name.to_s
       url.query = build_query(params)
